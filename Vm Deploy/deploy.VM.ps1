ed225c6fa15486cc6477988873400e26d46b4f47
Matthew Grimshaw <Matthew.Grimshaw@Microsoft.com>
2022-02-10 10:36:19 +0100

    dsc

---
+++ Vm Deploy/deploy.VM.ps1
@@ -0,0 +1,69 @@
+$subscriptionID = ''
+$artifactsStorageAccount = ''
+
+
+
+az login
+az account set --subscription $subscriptionID
+
+
+#Get SAS for Storage artificat
+
+$artifactsStorageKey = az storage account keys list `
+                       --account-name  $artifactsStorageAccount `
+                       --query [0].value `
+                       --output tsv
+
+## Get SAS Key for the Storage Account 
+$SasToken =                    az storage container generate-sas `
+                            --account-name $artifactsStorageAccount `
+                            --name $params.parameters.deploymentParameters.value.artifactsContainerName `
+                            --account-key $artifactsStorageKey `
+                            --permissions w `
+                            --output tsv
+
+    
+$connectionString = az storage account show-connection-string `
+                    --resource-group $resourceGroupName `
+                     --name $artifactsStorageAccount `
+                     --output tsv
+
+
+#Change Location
+Set-Location '.\AVDPoC\Vm Deploy'
+
+#Create zip archive for Deploy artifacts
+Compress-Archive -Path .\Configuration\DeployAgent\* -DestinationPath .\Configuration\DeployAgent.zip -Force
+
+#Remove Deploy Agent Folder
+Remove-Item .\Configuration\DeployAgent\ -Force -Recurse
+
+#Create zip archive for DSC artifacts
+Compress-Archive -Path .\Configuration\* -DestinationPath .\Configuration.zip -Force
+
+
+ ## upload artifacts to blob storage
+ az storage blob upload `
+    --name $artifactToUpload.name `
+    --container-name ($params.parameters.deploymentParameters.value.artifactsContainerName).ToLower() `
+    --file $artifactToUpload.Fullname `
+    --account-name $artifactsStorageAccount `
+    --connection-string $connectionString `
+    --sas-token $SasToken
+
+# Get SAS for dscArtifactsName
+$date = (Get-Date).AddMinutes(90).ToString("yyyy-MM-dTH:mZ")
+$date = $date.Replace(".",":")
+
+$dscArtifactsLocation =     az storage blob generate-sas `
+                            --account-name $artifactsStorageAccount `
+                            --container-name ($params.parameters.deploymentParameters.value.artifactsContainerName).ToLower() `
+                            --name $params.parameters.deploymentParameters.value.dscArtifactsName `
+                            --account-key $artifactsStorageKey `
+                            --permissions rw `
+                            --expiry $date `
+                            --full-uri `
+                            --output tsv
+
+
+write-host $dscArtifactsLocation
